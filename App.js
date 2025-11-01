0774baa4660d25c01709bc32b70d4d8302685bb8
Alexis Felipe Elorza Obregon <142292650+Elorza4A@users.noreply.github.com>
2025-10-31 22:44:15 -0600

    Add files via upload

--- App.js
+++ App.js
@@ -1,13 +1,69 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-import { estiloTextos } from './misEstilos';
+import { StyleSheet, Text, View, Button, TextInput, ListView, FlatList } from 'react-native';
+import { useState } from 'react';
 
 export default function App() {
+  const [contador, setContador] = useState(0);
+  const [nombre, setNombre] = useState('');
+  const [mensaje, setMensaje] = useState('');
+
+  const [listaAlumnos, setListaAlumnos] = useState([]);
+
+  function saludarAlumno(){
+    if(nombre.trim() === ''){
+      setMensaje('Escribe tu nombre');
+    }
+    else{
+      setMensaje(`Hola, ${nombre}`)
+    }
+  }
+
+  function contarCaracteres(){
+    if(nombre.length === 0){
+      setMensaje("🎃🎃🎃🎃🎃");
+    }
+    if (nombre.length <= 10){
+      setMensaje("👻👻👻👻👻")
+    }
+    if(nombre.length > 10 && nombre.length < 20){
+      setMensaje('🧛🧛🧛🧛🧛')
+    }
+  }
+
+
+  function agregarNombre() {
+    if (nombre.trim() === '')
+      return;
+    setListaAlumnos([...listaAlumnos, { nombre }]);
+    setNombre('');
+  }
+
+  function borrarAlumno(index) {
+    const nuevaLista = listaAlumnos.filter((_, i) => i !== index);
+    setListaAlumnos(nuevaLista);
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <Text style={estiloTextos.texto}>Hola, mundo</Text>
-      <StatusBar style="auto" />
+      <TextInput
+        placeholder='Escribe tu nombre'
+        value={nombre}
+        onChangeText={setNombre}
+        style={styles.input}
+      />
+
+      <Button title='Agregar alumno a la lista' onPress={agregarNombre} />
+
+      <FlatList
+        data={listaAlumnos}
+        keyExtractor={(item, index) => index.toString()}
+        renderItem={({ item, index }) => (
+          <View style={styles.item}>
+            <Text>{item.nombre}</Text>
+            <Button title="Borrar" onPress={() => borrarAlumno(index)} />
+          </View>
+        )}
+      />
     </View>
   );
 }
@@ -15,8 +71,24 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#000',
+    padding: 20,
+    marginTop: 40
+  },
+
+  input: {
+    borderWidth: 1,
+    borderColor: 'gray',
+    padding: 10,
+    marginBottom: 10,
+  },
+  
+  item: {
+    display: 'flex',
+    flexDirection: 'row',
+    justifyContent: 'space-between',
     alignItems: 'center',
-    justifyContent: 'center',
-  }
+    marginTop:10,
+    marginBottom: 8,
+  },
+
 });
